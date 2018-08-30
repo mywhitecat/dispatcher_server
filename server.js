00d970ec2d29a938f4d97b63b516d431e1dc0e03
Sviatoslav Vovdenko <svov@diceus.com>
2018-08-30 09:19:03 +0300

    Add server.

---
+++ server.js
@@ -0,0 +1,32 @@
+var io = require('socket.io').listen(18080); 
+// Отключаем вывод полного лога - пригодится в production'е
+// io.set('log level', 1);
+// Навешиваем обработчик на подключение нового клиента
+io.sockets.on('connection', function (socket) {
+    console.log('client connected');
+	// Т.к. чат простой - в качестве ников пока используем первые 5 символов от ID сокета
+	var ID = (socket.id).toString().substr(0, 5);
+	var time = (new Date).toLocaleTimeString();
+	// Посылаем клиенту сообщение о том, что он успешно подключился и его имя
+    socket.json.send({'event': 'connected', 'name': ID, 'time': time});    
+  
+	// Посылаем всем остальным пользователям, что подключился новый клиент и его имя
+	socket.broadcast.json.send({'event': 'userJoined', 'name': ID, 'time': time});
+	// Навешиваем обработчик на входящее сообщение
+	socket.on('message', function (msg) {
+
+        console.log(msg);
+
+		var time = (new Date).toLocaleTimeString();
+		// Уведомляем клиента, что его сообщение успешно дошло до сервера
+		socket.json.send({'event': 'messageSent', 'name': ID, 'text': msg, 'time': time});
+		// Отсылаем сообщение остальным участникам чата
+		socket.broadcast.json.send({'event': 'messageReceived', 'name': ID, 'text': msg, 'time': time})
+	});
+	// При отключении клиента - уведомляем остальных
+	socket.on('disconnect', function() {
+		var time = (new Date).toLocaleTimeString();
+        io.sockets.json.send({'event': 'userSplit', 'name': ID, 'time': time});
+        console.log('client disconnected');
+	});
+});
